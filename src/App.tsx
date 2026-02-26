a7525fba1e6b23e1650883cf72a231e4572b9f6f
Allan Brandão <allanbrandao@outlook.com.br>
2026-02-25 21:46:26 -0300

    chore: atualiza App.tsx para nova estrutura de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quos
-        autem, aperiam corrupti quia quaerat praesentium soluta quas labore
-        reiciendis, laudantium, quibusdam nemo. Asperiores blanditiis odio
-        accusamus ducimus optio nisi.
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
     </>
   );
 }
